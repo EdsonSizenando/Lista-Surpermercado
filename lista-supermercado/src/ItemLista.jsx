211a1388821d531598ace04b0b574e4402797843
EdsonSizenando <edson.sizenando@gmail.com>
2025-11-17 16:38:45 -0300

    logica finalizada

---
+++ lista-supermercado/src/ItemLista.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const ItemLista = ({itemLista, listaMercado, setListaMercado}) => {
+
+    const removerItemDaLista = () => {
+        const novaLista = [...listaMercado]
+
+        const novaListaFiltrada = novaLista.filter(
+            (itemAtual) => itemAtual != itemLista 
+        );
+
+        setListaMercado(novaListaFiltrada);
+
+    }
+    
+    return (
+        <li>
+            <p>{itemLista}</p>
+            <button onClick={() => removerItemDaLista()}>Remover</button>
+        </li>
+    );
+};
+
+export default ItemLista;
+
+// 'rafce' cria os componentes automaticamente
